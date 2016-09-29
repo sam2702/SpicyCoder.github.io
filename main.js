e089493d54f4eb28a94a68564f78b4c40c1094e1
SpicyCoder <spk2naveen@hotmail.com>
2016-09-29 18:03:28 +0530

    Initial Commit
    getting started

---
+++ main.js
@@ -0,0 +1,31 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 640, height: 480})
+  mainWindow.loadURL(`file://${__dirname}/weather.html`)
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
